bedfb171e9b11cf35e711886ee29db03fd8e0e70
Sharkao <76188187+Sharkao@users.noreply.github.com>
2021-02-28 22:59:42 -0300

    Add files via upload

---
+++ BancoVirtual/BancoVirtual/BancoVirtual/src/main/java/com/mycompany/bancovirtual/Conta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancovirtual;
+
+/**
+ *
+ * @author Pichau
+ */
+import java.util.Scanner;
+public class Conta {
+    private String nome;
+    private int conta, saques;
+    private double saldo;
+    Scanner entrada = new Scanner(System.in);
+}
---
+++ BancoVirtual/src/main/java/com/mycompany/bancovirtual/Conta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancovirtual;
+
+/**
+ *
+ * @author Pichau
+ */
+import java.util.Scanner;
+
+public class Conta {
+
+    private String nome;
+    private int conta, saques;
+    private double saldo;
+    Scanner entrada = new Scanner(System.in);
+
+    public Conta(String nome, int conta, double saldo_inicial) {
+        this.nome = nome;
+        this.conta = conta;
+        saldo = saldo_inicial;
+        saques = 0;
+    }
+}
